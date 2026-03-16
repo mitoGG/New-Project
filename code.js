4e4f0866fc1a6b1c3e4873383da05eabf713d945
mitoGG <jose.alves.siqueira@escola.pr.gov.br>
2026-03-16 10:44:45 -0300

    .

--- code.js
+++ code.js
@@ -1,5 +1,6 @@
-if (!username) {
-    document.querySelector("input[name='username']").classList.add("invalid");
-} else {
-    document.querySelector("input[name='username']").classList.remove("invalid");
+// Dentro do evento de envio do formulário, após a validação de campos
+let recaptchaResponse = grecaptcha.getResponse();
+if (!recaptchaResponse) {
+    alert("Por favor, complete o reCAPTCHA.");
+    event.preventDefault();
 }
